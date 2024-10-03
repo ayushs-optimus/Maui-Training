ae8b55f6f53d10437c4e32f08342d01f6e4dd9b5
Ayush Singh <ayushs-optimus>
2024-10-03 12:25:39 +0530

    updated files

---
+++ Projects/BMI Calculator/MVVM/ViewModel/BMIViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BMI_Calculator.MVVM.Models;
+
+namespace BMI_Calculator.MVVM.ViewModel;
+
+public class BMIViewModel
+{
+    public BMI BMI { get; set; }
+    public BMIViewModel()
+    {
+        BMI = new BMI
+        {
+            Height = 180,
+            Weight = 70
+        };
+    }
+}
